aa87f9f1580fe651d8e2272f02a5ea58994f323c
csanicola74 <caroline.sanicola@stonybrook.edu>
2022-08-29 22:38:39 -0400

    gcs

--- .history/app_20220829191506.py
+++ .history/app_20220829191506.py
@@ -27,7 +27,3 @@ def about():
 @app.route('/contact/')
 def contact():
     return '<h3>If you would like to contact us, please don\'t.</h3>'
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
